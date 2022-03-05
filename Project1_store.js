64db6d84bf5e22cc6f5d7489c17f773253d5453f
Eric Devitt <sailingrules@msn.com>
2022-03-05 11:17:47 -0500

    hamburger btn is functional

--- Project1_store.js
+++ Project1_store.js
@@ -2,3 +2,12 @@
 // function sale(){
 //     document.getElementById("banner").style.display="none";}
 //      setTimeout(sale,15000);
+
+// Navigation/Menu button
+
+// function toggleMenu(){
+//     document.getElementById("primaryNav").classList.toggle("open");
+// }
+// let x = document.getElementById("hamburgerBtn");
+// x.onclick=toggleMenu;
+
